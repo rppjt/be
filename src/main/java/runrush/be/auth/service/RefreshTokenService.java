776da0d6ce73dd1597f4b35859e1f447d7c62742
wontae <dnjsxo0616@gmail.com>
2025-05-18 20:45:06 +0900

    feat: 리프레시 토큰 갱신 메서드 추가 및 토큰 저장 로직 개선 #1

--- src/main/java/runrush/be/auth/service/RefreshTokenService.java
+++ src/main/java/runrush/be/auth/service/RefreshTokenService.java
@@ -1,7 +1,6 @@
 package runrush.be.auth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import runrush.be.auth.domain.RefreshToken;
@@ -15,26 +14,11 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
-    @Value("${jwt.refresh-token.expiration}")
-    private long jwtRefreshTokenExpiration;
-
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
     public Optional<RefreshToken> findByToken(String token) {
         return refreshTokenRepository.findByToken(token);
-    }
-
-    private void verifyExpiration(RefreshToken token) {
-        if (token.getExpiresAt().compareTo(Instant.now()) < 0) {
-            refreshTokenRepository.delete(token);
-            throw new RuntimeException("리프레시 토큰이 만료되었습니다. 다시 로그인해주세요");
-        }
-
-        if(!jwtTokenProvider.validateToken(token.getToken())) {
-            refreshTokenRepository.delete(token);
-            throw new RuntimeException("리프레시 토큰이 유효하지 않습니다. 다시 로그인해주세요.");
-        }
     }
 
     public String renewAccessToken(String token) {
@@ -47,7 +31,32 @@ public class RefreshTokenService {
     }
 
     @Transactional
+    public void renewRefreshToken(String email, String token, Instant expiresAt) {
+        refreshTokenRepository.deleteByUserEmail(email);
+
+        RefreshToken refreshToken = RefreshToken.builder()
+                .userEmail(email)
+                .token(token)
+                .expiresAt(expiresAt)
+                .build();
+
+        refreshTokenRepository.save(refreshToken);
+    }
+
+    @Transactional
     public void deleteRefreshToken(String userEmail) {
         refreshTokenRepository.deleteByUserEmail(userEmail);
+    }
+
+    private void verifyExpiration(RefreshToken token) {
+        if (token.getExpiresAt().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException("리프레시 토큰이 만료되었습니다. 다시 로그인해주세요");
+        }
+
+        if(!jwtTokenProvider.validateToken(token.getToken())) {
+            refreshTokenRepository.delete(token);
+            throw new RuntimeException("리프레시 토큰이 유효하지 않습니다. 다시 로그인해주세요.");
+        }
     }
 }
