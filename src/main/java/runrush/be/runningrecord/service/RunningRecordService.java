d77f32da88ee795a18246d7127ef33201376dee4
wontae <dnjsxo0616@gmail.com>
2025-05-20 17:25:19 +0900

    feat: 러닝 경로 기반 거리 계산 로직 및 저장 서비스 초기 구현 #6

---
+++ src/main/java/runrush/be/runningrecord/service/RunningRecordService.java
@@ -0,0 +1,61 @@
+package runrush.be.runningrecord.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import runrush.be.runningrecord.dto.RunningRecordRequest;
+import runrush.be.runningrecord.repository.RunningRecordRepository;
+import runrush.be.user.domain.User;
+import runrush.be.user.service.UserService;
+
+@Service
+@RequiredArgsConstructor
+public class RunningRecordService {
+    private final RunningRecordRepository runningRecordRepository;
+    private final UserService userService;
+
+    @Transactional
+    public void save(RunningRecordRequest request) throws JsonProcessingException {
+        User user = userService.findUserByEmail(request.email());
+    }
+
+
+    private double calculateTotalDistance(String pathGeoJson) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(pathGeoJson);
+        JsonNode coordinates = jsonNode.get("coordinates");
+
+        double totalDistance = 0.0;
+
+        for (int i = 1; i < coordinates.size(); i++) {
+            JsonNode prev = coordinates.get(i - 1);
+            JsonNode curr = coordinates.get(i);
+
+            double lon1 = prev.get(0).asDouble();
+            double lat1 = prev.get(1).asDouble();
+            double lon2 = curr.get(0).asDouble();
+            double lat2 = curr.get(1).asDouble();
+
+            totalDistance += haversine(lat1, lon1, lat2, lon2);
+        }
+
+        return totalDistance;
+    }
+
+    private double haversine(double lat1, double lon1, double lat2, double lon2) {
+        int R = 6371;
+
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c * 1000;
+    }
+}
