2e74af69d76044d4b71c9543c363a81832a70092
wontae <dnjsxo0616@gmail.com>
2025-05-21 18:16:50 +0900

    feat: 사용자 이메일 기반 러닝 기록 조회 메서드 추가 #6

--- src/main/java/runrush/be/runningrecord/repository/RunningRecordRepository.java
+++ src/main/java/runrush/be/runningrecord/repository/RunningRecordRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import runrush.be.runningrecord.domain.RunningRecord;
 
+import java.util.List;
+
 @Repository
 public interface RunningRecordRepository extends JpaRepository<RunningRecord, Long> {
+    List<RunningRecord> findByUserEmail(String email);
 }
