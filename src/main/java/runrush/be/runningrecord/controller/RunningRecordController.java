84359082ee44bae42f30b86adfeff8614f0c8a2e
wontae <dnjsxo0616@gmail.com>
2025-05-22 22:08:33 +0900

    refactor: 인증 객체를 User → UserPrincipal로 변경 #8

--- src/main/java/runrush/be/runningrecord/controller/RunningRecordController.java
+++ src/main/java/runrush/be/runningrecord/controller/RunningRecordController.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import runrush.be.auth.model.UserPrincipal;
 import runrush.be.runningrecord.dto.RunningRecordListResponse;
 import runrush.be.runningrecord.dto.RunningRecordRequest;
 import runrush.be.runningrecord.dto.RunningRecordResponse;
 import runrush.be.runningrecord.service.RunningRecordService;
-import runrush.be.user.domain.User;
 
 import java.util.List;
 
@@ -19,14 +19,14 @@ public class RunningRecordController {
     private final RunningRecordService runningRecordService;
 
     @PostMapping
-    public ResponseEntity<Void> createRunningRecord(@AuthenticationPrincipal User user,
-                                                             @RequestBody RunningRecordRequest request) {
+    public ResponseEntity<Void> createRunningRecord(@AuthenticationPrincipal UserPrincipal user,
+                                                    @RequestBody RunningRecordRequest request) {
         runningRecordService.saveRunningRecord(request, user.getEmail());
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<RunningRecordListResponse>> getRunningRecords(@AuthenticationPrincipal User user) {
+    public ResponseEntity<List<RunningRecordListResponse>> getRunningRecords(@AuthenticationPrincipal UserPrincipal user) {
         List<RunningRecordListResponse> runningRecords = runningRecordService.getRunningRecords(user.getEmail());
         return ResponseEntity.ok(runningRecords);
     }
