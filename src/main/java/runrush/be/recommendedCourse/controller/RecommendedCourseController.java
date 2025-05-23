f1a9d08f23dd4b9e4a3419906b0de03581c8c222
wontae <dnjsxo0616@gmail.com>
2025-05-23 17:21:13 +0900

    feat: 추천 코스 단건·목록 조회 API 추가 및 컨트롤러 리팩토링 #8

--- src/main/java/runrush/be/recommendedCourse/controller/RecommendedCourseController.java
+++ src/main/java/runrush/be/recommendedCourse/controller/RecommendedCourseController.java
@@ -6,8 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import runrush.be.auth.model.UserPrincipal;
+import runrush.be.recommendedCourse.dto.RecommendedCourseResponse;
 import runrush.be.recommendedCourse.dto.RecommendedCourseUpdateRequest;
 import runrush.be.recommendedCourse.service.RecommendedCourseService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/course")
@@ -17,7 +20,7 @@ public class RecommendedCourseController {
 
     @PostMapping("/{courseId}")
     public ResponseEntity<Void> createCourse(@PathVariable Long courseId,
-                                          @AuthenticationPrincipal UserPrincipal user) {
+                                             @AuthenticationPrincipal UserPrincipal user) {
         recommendedCourseService.createRecommendedCourse(courseId, user.getEmail(), user.getName());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
@@ -35,5 +38,23 @@ public class RecommendedCourseController {
                                              @AuthenticationPrincipal UserPrincipal user) {
         recommendedCourseService.deleteRecommendedCourse(courseId, user.getEmail());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<RecommendedCourseResponse> getCourse(@PathVariable Long courseId,
+                                                               @AuthenticationPrincipal UserPrincipal user) {
+        recommendedCourseService.getRecommendedCourse(courseId, user.getEmail());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<RecommendedCourseResponse>> getUserRecommendedCourses(@AuthenticationPrincipal UserPrincipal user) {
+        List<RecommendedCourseResponse> userRecommendedCourses = recommendedCourseService.getUserRecommendedCourses(user.getEmail());
+        return ResponseEntity.ok().body(userRecommendedCourses);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RecommendedCourseResponse>> getRecommendedCourses() {
+        return ResponseEntity.ok().body(recommendedCourseService.getRecommendedCourses());
     }
 }
