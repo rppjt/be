6ecc4fc744d641692e7ff9f39904b58dde03cb93
wontae <dnjsxo0616@gmail.com>
2025-05-25 02:07:43 +0900

    refactor: RecommendedCourseResponse에서 RunningRecord 제거#10

--- src/main/java/runrush/be/recommendedCourse/dto/RecommendedCourseResponse.java
+++ src/main/java/runrush/be/recommendedCourse/dto/RecommendedCourseResponse.java
@@ -1,13 +1,10 @@
 package runrush.be.recommendedCourse.dto;
 
 import runrush.be.recommendedCourse.domain.RecommendedCourse;
-import runrush.be.runningrecord.domain.RunningRecord;
-import runrush.be.user.domain.User;
 
 public record RecommendedCourseResponse(
         Long courseId,
         String userName,
-        RunningRecord runningRecord,
         String title,
         String description,
         String pathGeoJson,
@@ -19,7 +16,6 @@ public record RecommendedCourseResponse(
         return new RecommendedCourseResponse(
                 course.getId(),
                 course.getUser().getName(),
-                course.getRunningRecord(),
                 course.getTitle(),
                 course.getDescription(),
                 course.getPathGeoJson(),
