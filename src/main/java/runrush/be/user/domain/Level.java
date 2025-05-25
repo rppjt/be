fe9492622ef74a86c9fc355207e5bb6530fe895b
wontae <dnjsxo0616@gmail.com>
2025-05-25 19:38:52 +0900

    feat: 경험치 기반 레벨 판별 메서드 추가 및 마스터 레벨 정의 #12
    - MASTER(마스터 러너) 등급 추가 (1000 이상)
    - 경험치를 기반으로 레벨을 반환하는 fromExperiencePoints() 메서드 구현

--- src/main/java/runrush/be/user/domain/Level.java
+++ src/main/java/runrush/be/user/domain/Level.java
@@ -4,7 +4,8 @@ public enum Level {
     BEGINNER(1, "초보 러너", 0, 100),
     INTERMEDIATE(2, "열정 러너", 100, 300),
     ADVANCED(3, "숙련 러너", 300, 600),
-    EXPERT(4, "엘리트 러너", 600, 1000);
+    EXPERT(4, "엘리트 러너", 600, 1000),
+    MASTER(5, "마스터 러너", 1000, Integer.MAX_VALUE);
 
     private final int value;
     private final String title;
@@ -16,5 +17,14 @@ public enum Level {
         this.title = title;
         this.minExp = minExp;
         this.maxExp = maxExp;
+    }
+
+    public static Level fromExperiencePoints(int exp) {
+        for (Level level : Level.values()) {
+            if (exp >= level.minExp && exp < level.maxExp) {
+                return level;
+            }
+        }
+        return MASTER;
     }
 }
