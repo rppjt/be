1471b9239f1aee645d5d66b27b24a2ba21cca99f
wontae <dnjsxo0616@gmail.com>
2025-05-26 15:50:18 +0900

    feat: 사용자 ID 기반 즐겨찾기 목록 조회 기능 추가 #14

--- src/main/java/runrush/be/coursebookmark/repository/CourseBookmarkRepository.java
+++ src/main/java/runrush/be/coursebookmark/repository/CourseBookmarkRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import runrush.be.coursebookmark.domain.CourseBookmark;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CourseBookmarkRepository extends JpaRepository<CourseBookmark, Long> {
     Optional<CourseBookmark> findByUserIdAndCourseId(Long userId, Long courseId);
+    List<CourseBookmark> findByUserId(Long userId);
 }
