3d8b25870a0b72ab9e213a6f02449aaf6d47085b
wontae <dnjsxo0616@gmail.com>
2025-05-30 18:16:56 +0900

    refactor: 프론트와 변수명 매칭 작업 #20

--- src/main/java/runrush/be/runningrecord/dto/RunningRecordListResponse.java
+++ src/main/java/runrush/be/runningrecord/dto/RunningRecordListResponse.java
@@ -8,16 +8,18 @@ public record RunningRecordListResponse(
         Long id,
         double totalDistance,
         long totalTime,
+        double pace,
         String endLocationName,
-        LocalDateTime startedTime
+        LocalDateTime createAt
 ) {
     public static RunningRecordListResponse toRecordListResponse(RunningRecord record) {
         return new RunningRecordListResponse(
                 record.getId(),
                 record.getTotalDistance(),
                 record.getTotalTime(),
+                record.getPace(),
                 record.getEndLocationName(),
-                record.getStartedTime()
+                record.getCreatedAt()
         );
     }
 }
