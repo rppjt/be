cfc073aa7be4e24d1978105575161aa4431f9409
wontae <dnjsxo0616@gmail.com>
2025-06-04 00:58:44 +0900

    fix: 파라미터명 수정

--- src/main/java/runrush/be/courselike/repository/CourseLikeRepository.java
+++ src/main/java/runrush/be/courselike/repository/CourseLikeRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Repository
 public interface CourseLikeRepository extends JpaRepository<CourseLike, Long> {
 
-    @Query("SELECT l FROM CourseLike l WHERE l.user.id = :userId AND l.recommendedCourse.id = :corseId")
+    @Query("SELECT l FROM CourseLike l WHERE l.user.id = :userId AND l.recommendedCourse.id = :courseId")
     Optional<CourseLike> findByUserIdAndRecommendedCourseId(@Param("userId") Long userId,
                                                             @Param("courseId") Long courseId);
 
