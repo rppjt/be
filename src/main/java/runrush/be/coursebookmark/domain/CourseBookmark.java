74c25c31381b8188208313df92ae62d7a49bc14c
wontae <dnjsxo0616@gmail.com>
2025-06-09 14:38:01 +0900

    feat: CourseBookmark 엔티티에 북마크 생성일자 자동 설정 기능 추가 #31
    - @CreatedDate와 AuditingEntityListener를 적용하여 bookmarkedAt 필드 자동 관리

--- src/main/java/runrush/be/coursebookmark/domain/CourseBookmark.java
+++ src/main/java/runrush/be/coursebookmark/domain/CourseBookmark.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import runrush.be.recommendedCourse.domain.RecommendedCourse;
 import runrush.be.user.domain.User;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "course_bookmark")
 public class CourseBookmark {
     @Id
