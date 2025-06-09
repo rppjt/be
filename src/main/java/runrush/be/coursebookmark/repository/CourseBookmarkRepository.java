a5c212daae35a07030829a74e18c30c38b2ce864
wontae <dnjsxo0616@gmail.com>
2025-06-10 00:01:59 +0900

    refactor: @Modifying 애노테이션을 추가하여 북마크 삭제 쿼리 수정 #38

--- src/main/java/runrush/be/coursebookmark/repository/CourseBookmarkRepository.java
+++ src/main/java/runrush/be/coursebookmark/repository/CourseBookmarkRepository.java
@@ -1,6 +1,7 @@
 package runrush.be.coursebookmark.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,7 +20,9 @@ public interface CourseBookmarkRepository extends JpaRepository<CourseBookmark, 
             "ORDER BY cb.bookmarkedAt DESC")
     List<CourseBookmark> findWithCourseByUserId(Long userId);
 
-    void deleteByRecommendedCourseId(Long recommendedCourseId);
+    @Modifying
+    @Query("DELETE FROM CourseBookmark cb WHERE cb.recommendedCourse.id = :courseId")
+    void deleteByRecommendedCourseId(@Param("courseId") Long courseId);
 
     @Query("SELECT cb.recommendedCourse.id FROM CourseBookmark cb " +
             "WHERE cb.user.id = :userId")
