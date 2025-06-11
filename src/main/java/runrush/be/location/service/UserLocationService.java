36aa5c6e88af34765673335c9a0bf3f000e2dfd4
wontae <dnjsxo0616@gmail.com>
2025-06-11 21:22:11 +0900

    feat: 사용자 위치 등록 및 50m 이상 이동 시 위치 갱신 기능 추가 #43

--- src/main/java/runrush/be/location/service/UserLocationService.java
+++ src/main/java/runrush/be/location/service/UserLocationService.java
@@ -8,7 +8,9 @@ import runrush.be.common.exception.BusinessException;
 import runrush.be.common.exception.ErrorCode;
 import runrush.be.location.domain.UserLocation;
 import runrush.be.location.dto.LocationSharingResponse;
+import runrush.be.location.dto.LocationUpdateRequest;
 import runrush.be.location.repository.UserLocationRepository;
+import runrush.be.user.domain.User;
 import runrush.be.user.service.UserService;
 
 import java.util.Optional;
@@ -37,6 +39,34 @@ public class UserLocationService {
                     : LocationSharingResponse.disabled(userId);
         } else {
             throw new BusinessException(ErrorCode.LOCATION_UPDATE_REQUIRED, "위치 정보가 없습니다. 먼저 위치를 업데이트해주세요.");
+        }
+    }
+
+    @Transactional
+    public void updateLocation(Long userId, LocationUpdateRequest request) {
+        Optional<UserLocation> existingLocation = userLocationRepository.findById(userId);
+
+        if (existingLocation.isPresent()) {
+            UserLocation userLocation = existingLocation.get();
+
+            if (userLocation.hasSignificantMovement(request.latitude(), request.longitude())) {
+                userLocation.updateLocation(request.latitude(), request.longitude());
+                userLocationRepository.save(userLocation);
+                log.info("사용자 {} 위치 업데이트: ({}, {})", userId, request.latitude(), request.longitude());
+            } else {
+                log.debug("사용자 {} 움직임이 미미하여 위치 업데이트 생략 (50m 미만)", userId);
+            }
+        } else {
+            User user = userService.findUserById(userId);
+            UserLocation newLocation = UserLocation.builder()
+                    .user(user)
+                    .latitude(request.latitude())
+                    .longitude(request.longitude())
+                    .isSharing(true) // 기본값: 위치 공유 허용
+                    .build();
+
+            userLocationRepository.save(newLocation);
+            log.info("사용자 {} 첫 위치 등록: ({}, {})", userId, request.latitude(), request.longitude());
         }
     }
 }
