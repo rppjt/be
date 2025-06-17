38a59336ad1f85d04b04a9e1a7490211cc1272f7
wontae <dnjsxo0616@gmail.com>
2025-06-17 20:11:45 +0900

    feat: deploy 배포 확인용 추가 #49

--- src/main/java/runrush/be/config/SecurityConfig.java
+++ src/main/java/runrush/be/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/health-check").permitAll()
                         .requestMatchers("/oauth2/**", "/login/**", "/auth/token", "/auth/refresh").permitAll()
                         .anyRequest().authenticated()
                 )
