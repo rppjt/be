02d655b305960b49340f64e734e2b975ee968a4d
wontae <dnjsxo0616@gmail.com>
2025-06-26 15:01:20 +0900

    refactor: config 수정

--- src/main/java/runrush/be/config/SecurityConfig.java
+++ src/main/java/runrush/be/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOriginPatterns(List.of("*")); // 더 유연한 설정
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization", "Set-Cookie")); // Set-Cookie 추가
